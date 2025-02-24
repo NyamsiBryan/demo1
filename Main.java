50d9588e3958dba7a87466e5fbce0da19bc3a791
NyamsiBryan <bnyamsi@my.okcu.edu>
2025-02-24 12:42:50 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,5 @@ public class Main {
 public static void main(String [] args) {
    System.out.println("Hello from feature01");
    System.out.println("Hello World");
-   System.out.println(“World Hello”);
 }
 }
